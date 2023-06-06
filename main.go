5509358527b7469da9373865f320fd3d9d81d78e
LucaBazzea <luca.bazzea@gmail.com>
2023-06-06 17:56:13 +0200

    Sync with desktop

--- main.go
+++ main.go
@@ -1,35 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 )
 
-type Quote struct {
-	Text [1]string `json:"Dwight_Schrute"`
-}
-
-func loadQuote(filename string) (Quote, error) {
-	var quote Quote
-
-	quoteFile, err := os.Open(filename)
-	defer quoteFile.Close()
-
-	if err != nil {
-		return quote, err
-	}
-
-	jsonParser := json.NewDecoder(quoteFile)
-	err = jsonParser.Decode(&quote)
-
-	return quote, err
-}
 
 func main() {
-	fmt.Println("Opened JSON file")
+  dir, error := os.ReadDir("quotes")
 
-	quote, _ := loadQuote("schrute.json")
-
-	fmt.Println(quote)
+  if error != nil {
+    fmt.Println("Error: ", error)
+    return
+  }
+  
+  for _, entry := range dir {
+    fmt.Println(entry.Name())
+  }
 }
